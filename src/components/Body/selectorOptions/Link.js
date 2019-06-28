e1cfaebef20bb45a4123f179a5ed3083d2b61b87
Oldmo <35811214+105306006@users.noreply.github.com>
2019-06-29 02:07:18 +0800

    mo ui update

--- src/components/Body/selectorOptions/Link.js
+++ src/components/Body/selectorOptions/Link.js
@@ -7,7 +7,8 @@ class Link extends React.Component {
     super(props);
     this.state = {
       content: '',
-      remark: ''
+      remark: '',
+      delHover: false
     }
   }
   componentDidMount(){
@@ -57,7 +58,13 @@ class Link extends React.Component {
       color: '#CBCBCB'
     }
   return (
-      <div className="optionStyle">
+      <div className="optionStyle" onMouseOver={() => {
+        this.setState({ delHover: true })
+      }}
+
+      onMouseOut={() => {
+        this.setState({ delHover: false})
+      }}>
         <div style={{position: "relative",width:'100%'}}>
             <div style={headerStyle}>
                 <p><img src="https://s3-ap-northeast-1.amazonaws.com/www.memepr.com/smartQA/icon_link.png" style={{width:'15px',height: '15px',marginRight: '7px'}}></img>超連結訊息</p>
@@ -67,7 +74,11 @@ class Link extends React.Component {
                 <input placeholder="在此輸入文字註解" style={{height: '64px',fontSize:'12px',color:'#101010',border:'solid 1px #F0F0F0',padding: '16px'}} onChange={this.handleRemarkChange} value={this.state.remark}></input>
             </div>
         </div>
-        <div className="rwd_cleanup_btn inside_div">
+        <div className="rwd_cleanup_btn inside_div" style={{
+          visibility: this.state.delHover ? 'visible' : 'hidden',
+          opacity: this.state.delHover ? 1 : 0,
+          transition: ' 0.5s '
+      }}>
             <button
               className="cleanup_btn"
               style={{ marginLeft: "15px", marginTop: "-1px",width:"24px",height: "24px",padding: "2px"}}
